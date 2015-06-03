4c03fa3306a3c5a387654f86e401cc418b0d1932
niemin <506936759@qq.com>
2015-06-03 09:16:05 +0800

    售后记录

--- src/com/example/zf_pad/trade/AfterSaleMarkActivity.java
+++ src/com/example/zf_pad/trade/AfterSaleMarkActivity.java
@@ -2,14 +2,15 @@ package com.example.zf_pad.trade;
 
 import static com.example.zf_pad.fragment.Constants.AfterSaleIntent.RECORD_ID;
 import static com.example.zf_pad.fragment.Constants.AfterSaleIntent.RECORD_TYPE;
-
 import android.app.Activity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+
 
 
 import com.example.zf_pad.BaseActivity;
@@ -28,7 +29,7 @@ public class AfterSaleMarkActivity extends BaseActivity {
 
 	private EditText mCompanyEdit;
 	private EditText mNumberEdit;
-	private Button mSubmitBtn;
+	private Button mSubmitBtn,close;
 
 	private final TextWatcherAdapter mTextWatcher = new TextWatcherAdapter() {
 
@@ -46,10 +47,18 @@ public class AfterSaleMarkActivity extends BaseActivity {
 		setContentView(R.layout.activity_after_sale_mark);
 		//new TitleMenuUtil(this, getString(R.string.title_after_sale_mark)).show();
 
+		close = (Button) findViewById(R.id.close);
 		mCompanyEdit = (EditText) findViewById(R.id.after_sale_mark_company);
 		mNumberEdit = (EditText) findViewById(R.id.after_sale_mark_number);
 		mSubmitBtn = (Button) findViewById(R.id.after_sale_mark_submit);
 
+		close.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				finish();
+			}
+		});
 		mCompanyEdit.addTextChangedListener(mTextWatcher);
 		mNumberEdit.addTextChangedListener(mTextWatcher);
 		mSubmitBtn.setOnClickListener(new View.OnClickListener() {
